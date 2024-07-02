4b06a375cbfea486645218c40df7e264f67f9556
Yesid Cano <yesidcanoc@gmail.com>
2024-07-02 16:25:34 +0200

    updated start page messages

--- start.py
+++ start.py
@@ -29,12 +29,14 @@ start_message = """
 
 - **This tool is a temporary test server meant for gathering feedback over a few days.**
 - **It is intended for internal use only and not for public access.**
-- The German version is still in progress and may not provide results as accurate as the English version (I suggest that you first use the English version).
 - With this test I am interested in finding out when the chat usually hallucinates and how often it hallucinates.
 - I am also interested in getting a sense of how often the chatbot is able to provide the correct answer to the user's question.
 
 Kindly note that any information you input or search for in the chat area is sent to OpenAI.
 The Chatbot is powered by gpt-4o.
+
+Have any question? [Email Me](mailto: yecanocastro@uos.de)
+
 """
 
 if "selected_language" in st.session_state:
@@ -45,12 +47,13 @@ if "selected_language" in st.session_state:
 
 - Bei diesem Tool handelt es sich um einen temporären Testserver, mit dem wir über einige Tage hinweg Feedback einholen können.
 - **Es ist nur für den internen Gebrauch und nicht für den öffentlichen Zugang bestimmt.**
-- **Die deutsche Version ist noch in Arbeit und liefert möglicherweise nicht so genaue Ergebnisse wie die englische Version (ich empfehle, zunächst die englische Version zu verwenden).**
 - Mit diesem Test möchte ich herausfinden, wann der Chat normalerweise halluziniert und wie oft er halluziniert.
 - Ich bin auch daran interessiert, ein Gefühl dafür zu bekommen, wie oft der Chatbot in der Lage ist, die richtige Antwort auf die Frage des Benutzers zu geben.
 
 Bitte beachten Sie, dass alle Informationen, die Sie in den Chatbereich eingeben oder nach denen Sie suchen, an OpenAI gesendet werden.
 Der Chatbot wird von gpt-4o betrieben.
+
+Bei Fragen, melde dich bei mir [Email Me](mailto: yecanocastro@uos.de)
 """
 
 initialize_language()
