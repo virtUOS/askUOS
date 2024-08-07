adadc7e92ec778a65487f4f4e8b4407fcb00041d
Yesid Cano <yesidcanoc@gmail.com>
2024-08-07 12:47:15 +0000

    refactored the search_web_tool.py and chatbot_logger.py scripts

--- src/chatbot/tools/search_web_tool.py
+++ src/chatbot/tools/search_web_tool.py
@@ -1,6 +1,5 @@
 import re
 import urllib.parse
-import logging
 
 import dotenv
 import requests
@@ -20,27 +19,32 @@ from config.settings import SEARCH_URL, SERVICE
 dotenv.load_dotenv()
 
 
-# TODO summarize  the content when it + the prompt +chat_history exceed the number of openai allow tokens (16385 tokens)
+MAX_NUM_LINKS = 2
+HEADLESS_OPTION = "--headless"
+QUERY_SPACE_REPLACEMENT = "+"
+
+
+# TODO summarize the content when it + the prompt +chat_history exceed the number of openai allowed tokens (16385 tokens)
 def summarise_content(text, question):
-    print(
-        "---------------------------------------------------------summarising content...---------------------------------------------------------"
-    )
+    logger.info("Summarizing content...")
     llm = ChatOpenAI(temperature=0)
+
     text_splitter = RecursiveCharacterTextSplitter(
         separators=["\n\n", "\n"], chunk_size=3500, chunk_overlap=300
     )
     docs = text_splitter.create_documents([text])
 
-    reduce_template_string = """I will act as a text summarizer to help create a concise summary of the text provided, with a focus on addressing the given query. 
-    The summary can be up to 20 sentences in length, capturing the key points and concepts from the original text without adding interpretations. The summary should keep the links found in the text. 
-    Irrelevant details not related to the question will be excluded from the summary.
+    reduce_template_string = """I will act as a text summarizer to help create a concise summary of the text provided, with a focus on addressing the given query.
+The summary can be up to 20 sentences in length, capturing the key points and concepts from the original text without adding interpretations. The summary should keep the links found in the text.
+Irrelevant details not related to the question will be excluded from the summary.
 
-   Summarize this text:
-        {text}
+Summarize this text:
+{text}
 
-        question: {question}
-        Answer:
-    """
+question: {question}
+Answer:
+"""
+
     reduce_template = PromptTemplate(
         template=reduce_template_string, input_variables=["text", "question"]
     )
@@ -51,125 +55,111 @@ def summarise_content(text, question):
         combine_prompt=reduce_template,
         verbose=True,
     )
-    summary_result_text = chain.run(input_documents=docs, question=question)
-    # return chain.run(input_documents=docs, question=question)
-    return summary_result_text
+
+    return chain.run(input_documents=docs, question=question)
 
 
+def log_search_query(func):
+    def wrapper(*args, **kwargs):
+        query = func(*args, **kwargs)
+        logger.info(f"This is the query used by the University website: {query}")
+        return query
+
+    return wrapper
+
+
+@log_search_query
 def decode_string(query):
     """
-    Decode the query string to a format that can be used by the university website. e.g., wo+ist+der+universität
-    :return: decoded string in this format: wo+ist+der+universität
+    Decode the query string to a format that can be used by the university website.
     """
 
-    # Regular expressions for checking different types of encoding
     utf8_pattern = re.compile(
         b"[\xc0-\xdf][\x80-\xbf]|[\xe0-\xef][\x80-\xbf]{2}|[\xf0-\xf7][\x80-\xbf]{3}"
     )
     url_encoding_pattern = re.compile(r"%[0-9a-fA-F]{2}")
     unicode_escape_pattern = re.compile(r"\\u[0-9a-fA-F]{4}")
 
-    # Check for UTF-8 encoding
-    if utf8_pattern.search(query.encode("latin1")):
-        try:
-            decoded_utf8 = query.encode("latin1").decode("utf-8")
-            logger.info(
-                f"This is the query used by the University website: {decoded_utf8}"
+    try:
+        # Check for URL encoding
+        if utf8_pattern.search(query.encode("latin1")):
+            return (
+                query.encode("latin1")
+                .decode("utf-8")
+                .replace(" ", QUERY_SPACE_REPLACEMENT)
             )
-
-            return decoded_utf8.replace(" ", "+")
-        except Exception as e:
-            pass
-
-    # Check for URL encoding
-    if url_encoding_pattern.search(query):
-        try:
-            decoded_url = urllib.parse.unquote(query)
-            logger.info(
-                f"This is the query used by the University website: {decoded_url}"
+        if url_encoding_pattern.search(query):
+            return urllib.parse.unquote(query).replace(" ", QUERY_SPACE_REPLACEMENT)
+        if unicode_escape_pattern.search(query):
+            return (
+                query.encode("latin1")
+                .decode("unicode-escape")
+                .replace(" ", QUERY_SPACE_REPLACEMENT)
             )
+    except Exception as e:
+        logger.error(f"Error decoding query string: {e}")
 
-            return decoded_url.replace(" ", "+")
-        except Exception as e:
-            pass
-
-    # Check for Unicode escape sequences
-    if unicode_escape_pattern.search(query):
-        try:
-            decoded_unicode = query.encode("latin1").decode("unicode-escape")
-            logger.info(
-                f"This is the query used by the University website: {decoded_unicode}"
-            )
-
-            return decoded_unicode.replace(" ", "+")
-        except Exception as e:
-            pass
-
-    query = query.replace(" ", "+")
-    logger.info(f"This is the query used by the University website: {query}")
-
-    return query  # Return the original string if no decoding is needed
+    # return query  # Return the original string if no decoding is needed
+    return query.replace(" ", QUERY_SPACE_REPLACEMENT)
 
 
-def extract_and_visit_links(rendered_html: str, max_num_links: int = 2):
-    """
-    Extract the content from the search result  and visit the links to extract the content.
-    :param rendered_html: The rendered HTML of the search result page (e.g., a set of links).
-    :param max_num_links: The number of links to visit and extract the content from.
-    """
+def extract_pdf_text(href: str, response) -> str:
+
+    text = read_pdf_from_url(response)
+    return re.sub(r"(\n\s*|\n\s+\n\s+)", "\n", text.strip())
+
+
+def extract_html_text(href: str, response) -> str:
+
+    link_soup = BeautifulSoup(response.content, "html.parser")
+    div_content = link_soup.find("div", class_="eb2")
+    if div_content:
+        text = re.sub(r"\n+", "\n", div_content.text.strip())
+        content_with_link = ""
+        for link in div_content.find_all("a", href=True):
+            text_anchor_tag = re.sub(r"\n+", "\n", link.text.strip())
+            content_with_link += f" - {text_anchor_tag}: {link['href']}"
+        return text + "\nHref found in the text:\n" + content_with_link
+    logger.error(f"Failed to fetch html content from: {href}")
+    return ""
+
+
+def extract_and_visit_links(rendered_html: str, max_num_links: int = MAX_NUM_LINKS):
     contents = []
     taken_from = "Information taken from:"
     search_result_text = "Content not found"
     visited_links = set()
     soup = BeautifulSoup(rendered_html, "html.parser")
-    # 'gs-title' is the class of the anchor tag that contains the search result (University website search result page)
-    anchor_tags = set(soup.find_all("a", class_="gs-title"))  # search result links
+    # 'gs-title' is the class attached to the anchor tag that contains the search result (University website search result page)
+    anchor_tags = set(soup.find_all("a", class_="gs-title"))  # the search result links
+
     for tag in anchor_tags:
-        href = tag.get("href")
-        response = requests.get(href)
+        href = str(tag.get("href"))
+        # there could be repeated links
+        if href in visited_links:
+            continue
+        try:
+            response = requests.get(href)
+        except:
+            logger.error(f"Error while fetching: {href}")
+            continue
+
         if response.status_code == 200:
-            visited_links.add(href)
-            # if the link is a pdf file
+
             if href.endswith(".pdf"):
-                text = read_pdf_from_url(response)
-                text = re.sub(
-                    r"(\n\s*|\n\s+\n\s+)", "\n", text.strip()
-                )  # remove extra spaces
-                logger.info(f"Information extracted from pdf file: {href}")
-
+                text = extract_pdf_text(href, response)
             else:
+                text = extract_html_text(href, response)
 
-                # TODO use the firefox driver to visit the link and extract the content
-                link_soup = BeautifulSoup(response.content, "html.parser")
-                # 'eb2' is the class of the div tag that contains the content of the search result (University website)
-                div_content = link_soup.find("div", class_="eb2")
-                if div_content:
-                    text = re.sub(r"\n+", "\n", div_content.text.strip())
+            if text:
+                text = f"{taken_from}{href}\n{text}"
+                contents.append(text)
+                visited_links.add(href)
 
-                    content_with_link = ""
+                if len(visited_links) >= max_num_links:
+                    break
 
-                    for link in div_content.find_all("a", href=True):
-                        text_anchor_tag = re.sub(
-                            r"\n+", "\n", link.text.strip()
-                        )  # Extract the text
-                        content_with_link += f" - {text_anchor_tag}: {link['href']}"  # Combine the text and the link
-
-                    text += "\nHref found in the text:\n" + content_with_link
-
-                else:
-                    contents.append(f"Failed to fetch content from the link: {href}")
-                    print(f"Failed to fetch content from the link: {href}")
-
-            text = f"{taken_from}{href}\n{text}"
-            contents.append(text)
-
-        # only visit the first two links
-        if len(visited_links) >= max_num_links:
-            break
-
-    search_result_text = "\n".join(contents)
-
-    return search_result_text
+    return "\n".join(contents) if contents else search_result_text
 
 
 def search_uni_web(query: str) -> str:
@@ -186,36 +176,34 @@ def search_uni_web(query: str) -> str:
         - This function is specifically designed to handle questions about the University of Osnabrück, such as the application process or studying at the university.
 
     """
+
     try:
-        # TODO need to check if the query is in German, if not then translate it to German
-        query = query
-        print(f"------Query--------: {query}")
+
         query_url = decode_string(query)
-        print(f"------Query URL--------: {query_url}")
-        # constructs the complete URL for the search
+        logger.info(f"Decoded query URL: {query_url}")
+
         firefox_options = Options()
-        firefox_options.add_argument("--headless")  # Run Firefox in headless mode
-        service = Service(SERVICE)  # path to the geckodriver
+        firefox_options.add_argument(HEADLESS_OPTION)
+        service = Service(SERVICE)
         driver = webdriver.Firefox(service=service, options=firefox_options)
+
         url = SEARCH_URL + query_url
         driver.get(url)
         rendered_html = driver.page_source
-
         search_result_text = extract_and_visit_links(rendered_html)
-        logger.info(f"Length of the search result text: {len(search_result_text)}")
+
         # TODO use algorithm from my thesis to compute exact number of tokens given length of the search result text
-        # truncate the search result text to 15000 tokens
         if len(search_result_text) > 15000:
-            print(
-                f"-------------------------------Truncate. length of the original search result text-------------------------: {len(search_result_text)}"
+            logger.info(
+                f"Truncating search result text due to length: {len(search_result_text)}"
             )
             search_result_text = search_result_text[:15000]
 
         driver.quit()
         return search_result_text
     except Exception as e:
-        logger.error(f"Error while searching the web: {e}")
-        print(f"Error while searching the web: {e}")
-        # raise ToolException('Error while searching the web')
+        logger.error(f"Error while searching the web: {e}", exc_info=True)
         driver.quit()
         return "Error while searching the web"
+
+
--- src/chatbot_log/chatbot_logger.py
+++ src/chatbot_log/chatbot_logger.py
@@ -2,21 +2,38 @@ import logging
 import csv
 import sys
 
+
 class CsvFormatter(logging.Formatter):
     def format(self, record):
         created = self.formatTime(record, "%Y-%m-%d %H:%M:%S")
-        return f"{created},{record.name},{record.levelname},{record.getMessage()}"
+        return f"{created},{record.pathname},{record.lineno},{record.name},{record.levelname},{record.getMessage()}"
+
+
+class CsvFilter(logging.Filter):
+    def filter(self, record):
+        # save only WARNING and above levels:
+        return record.levelno >= logging.WARNING
 
 
 # Create a logger
-logger = logging.getLogger('chatbot_logger')
+logger = logging.getLogger("chatbot_logger")
 logger.setLevel(logging.DEBUG)  # Set the logging level
 
 
 # Create a CSV file handler and set the formatter
-csv_handler = logging.FileHandler('log.csv')
+csv_handler = logging.FileHandler("log.csv")
 csv_handler.setLevel(logging.DEBUG)
 csv_formatter = CsvFormatter()
 csv_handler.setFormatter(csv_formatter)
+csv_handler.addFilter(CsvFilter())
 logger.addHandler(csv_handler)
 
+
+# Create a console handler and set the formatter
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+console_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+# console_handler.setFormatter(console_formatter)
+# logger.addHandler(console_handler)
