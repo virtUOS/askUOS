3ba8fb5847e1818a5cdc7a8bce7cfee505943482
Yesid Cano <yesidcanoc@gmail.com>
2024-12-08 14:40:50 +0000

    added new feedback mechanism

--- pages/streamlit_chat.py
+++ pages/streamlit_chat.py
@@ -9,6 +9,7 @@ from src.chatbot.agents.agent_openai_tools import CampusManagementOpenAIToolsAge
 from src.chatbot.tools.utils.tool_helpers import visited_links
 from src.chatbot_log.chatbot_logger import logger
 from pages.utils import initialize_session_sate
+from streamlit_feedback import streamlit_feedback
 
 # create an instance of the agent executor
 # TODO every time the users interacts with the chatbot, all the script  re-runS. This is not efficient. CACHE THE AGENT EXECUTOR??? (solved singltone pattern)
@@ -74,13 +75,14 @@ if prompt := st.chat_input(placeholder=session_state["_"]("Message")):
         st.write(prompt)
 
 
-def get_feedback(user_query, response, time_taken, rate):
+if session_state.user_feedback:
     feedback = {
-        "user_query": user_query,
-        "response": response,
-        "time_taken": time_taken,
-        "rate": rate,
+        "user_query": session_state.user_query,
+        "response": session_state.messages[-1]["content"],
+        "time_taken": session_state.time_taken,
+        "rate": session_state.user_feedback["score"],
     }
+    session_state.user_feedback = None
     logger.info(f"Feedback= {feedback}")
 
 
@@ -94,17 +96,11 @@ if st.session_state.messages[-1]["role"] != "assistant":
             start_time = time.time()
             settings.time_request_sent = start_time
 
-            # chunks = []
-            # for chunk in agent_executor._agent_executor.stream({"input": prompt}):
-            #     chunks.append(chunk)
-            #     st.write(chunk)
-
-            # streaming is done in CallbackHandlerStreaming
             response = agent_executor(prompt)
-            # response = agent.invoke({"input": prompt})
-            # end time
+
             end_time = time.time()
             time_taken = end_time - start_time
+            session_state["time_taken"] = time_taken
             logger.info(
                 f"Time taken to serve whole answer to the user: {time_taken} seconds"
             )
@@ -118,48 +114,12 @@ if st.session_state.messages[-1]["role"] != "assistant":
                         st.write("- " + link)
                 visited_links.clear()
 
-            # if "sources" in response:
-            #     if response["sources"]:
-            #         with st.expander("Sources"):
-            #             for source in response["sources"]:
-            #                 st.write(f"- {source}")
-
-            # Comment out the following code block to disable the PDF download feature
-            # pdf_content, pdf_file_name = extract_pdf_with_timeout(
-            #     response["output"], 15
-            # )
-            # if pdf_content:
-
-            #     st.download_button(
-            #         label=f"Download: {pdf_file_name}",
-            #         data=pdf_content,
-            #         file_name=pdf_file_name,
-            #         mime="application/pdf",
-            #     )
-
-    col1, col2, col3, col4 = st.columns([3, 3, 0.5, 0.5])
-
-    with col3:
-        with st.container(key="FeedbackLike"):
-            st.button(
-                label=":thumbsup:",
-                type="primary",
-                on_click=get_feedback,
-                key="like-button",
-                args=[prompt, response["output"], time_taken, "like"],
-                help=session_state["_"]("Click here if you like the response"),
-            )
-
-    with col4:
-        with st.container(key="FeedbackDislike"):
-            st.button(
-                ":thumbsdown:",
-                on_click=get_feedback,
-                type="primary",
-                key="dislike-button",
-                args=[prompt, response["output"], time_taken, "dislike"],
-                help=session_state["_"]("Click here if you dislike the response"),
-            )
+        st.session_state.user_query = prompt
+        feedback = streamlit_feedback(
+            feedback_type="faces",
+            key="user_feedback",
+            # optional_text_label="[Optional] Please provide an explanation",
+        )
 
     message = {
         "role": "assistant",
--- pages/utils.py
+++ pages/utils.py
@@ -7,7 +7,15 @@ def initialize_session_sate() -> None:
     Initializes the session state with default values if they are not already set.
     """
     # Initialization
-    defaults = {"_": translate(), "selected_language": "Deutsch", "show_warning": True}
+    defaults = {
+        "_": translate(),
+        "selected_language": "Deutsch",
+        "show_warning": True,
+        "user_feedback": None,
+        "user_query": None,
+        "response": None,
+        "time_taken": None,
+    }
 
     for key, value in defaults.items():
         if key not in session_state:
