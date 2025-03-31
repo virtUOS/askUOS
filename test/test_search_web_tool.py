53b03d8b0e22bf005cf5c51cfebfc246f2d7e102
yesidc <yesidcanoc@gmail.com>
2025-03-31 09:12:35 +0000

    adds tests for ui and web_search_tool

---
+++ test/test_search_web_tool.py
@@ -0,0 +1,38 @@
+import sys
+import unittest
+from unittest.mock import MagicMock, patch
+
+# print(f'Python version: {sys.path}')
+
+sys.path.append("./")
+from src.chatbot.tools.search_web_tool import SearchUniWebTool
+
+
+class TestWebSearch(unittest.TestCase):
+    def setUp(self):
+        self.search_tool = SearchUniWebTool()
+
+    @patch("src.chatbot.tools.search_web_tool.settings")
+    # receives the mock objects as parameters in the reverse order of their respective @patch decorators
+    def test_generate_summary(self, mock_settings):
+        # Mock settings
+        mock_settings.model.context_window = 1000
+
+        # Input data (Text generated with gpt)
+        text = """
+        Applying to a university is an important step in pursuing higher education and can often be a complex process. The first phase involves researching potential universities and programs to determine the best fit for your academic and career goals. Once you have a list of prospects, you'll need to gather necessary materials, such as transcripts, letters of recommendation, and, in some cases, standardized test scores. Crafting a compelling personal statement or essay is another critical component, where you can showcase your achievements, aspirations, and unique qualities. It's important to pay close attention to application deadlines and to ensure that all materials are submitted on time. Financial aid and scholarships may also require separate applications, so it's essential to be organized and thorough. After submitting your applications, the waiting period begins, culminating in decisions that could shape the next chapter of your educational journey.
+        """
+        question = "What is the main point?"
+
+        # Call the method
+        result = self.search_tool.generate_summary(text, question)
+
+        self.assertLess(
+            len(result),
+            len(text),
+            "The summary should be shorter than the provided text",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
--- test/test_streamlit_chat.py
+++ test/test_streamlit_chat.py
@@ -1,39 +1,44 @@
-from streamlit.testing.v1 import AppTest
-from langchain.evaluation import load_evaluator
+import sys
 import time
 import unittest
 
+from langchain.evaluation import load_evaluator
+from streamlit.testing.v1 import AppTest
+
+sys.path.append("./")
 
 
 class BaseTestStreamlitApp(unittest.TestCase):
-    
 
     def test_output(self):
 
-        at = AppTest.from_file('pages/streamlit_chat.py', default_timeout=30).run()
+        at = AppTest.from_file("/app/pages/ask_uos_chat.py", default_timeout=30).run()
 
-        at.chat_input[0].set_value('can I study Biology?').run()
+        at.chat_input[0].set_value("can I study Biology?").run()
 
         # test memory
-        at.chat_input[0].set_value('how long does the Master take?').run()
+        at.chat_input[0].set_value("how long does the Master take?").run()
 
+        # Test references
+        self.assertGreaterEqual(len(at.expander[0]), 1)
         time.sleep(5)
-        
+
         # test output
         expected_output = "The Master's program in Biology at the University of Osnabrück has a standard study period of 4 semesters. The language of instruction is primarily English, with some courses offered in German. The program provides a research-oriented specialization in current areas of molecular and organismic biology, covering a broad methodological and thematic spectrum. This includes topics ranging from structural biology and biophysical fundamentals to cell biology, physiological phenomena, ecological, evolutionary, and behavioral biological questions.\n\nThe Master's program offers three thematic focal points: (1) General Biology, (2) Evolution, Behavior & Ecology, and (3) Cell and Molecular Biology. It also includes flexible module options within the chosen focus, interdisciplinary skills development, a strong practical component with a research focus, and the opportunity to conduct thesis work in an excellent research environment with access to state-of-the-art laboratory infrastructure.\n\nUpon completion of the Master's program, graduates have diverse career opportunities in areas such as fundamental research at universities and other research institutions, applied research, development, and distribution (e.g., pharmaceutical or agro-industry), teaching in an academic environment, science journalism, work as an expert (e.g., state criminal investigation department, environmental assessments), employment in museums, zoological or botanical gardens, healthcare, and public administration.\n\nFor further information about the Master's program, access to study plans, admission requirements, and application procedures, you can contact the academic advising office for Biology or the student representatives in the Biology student council.\n\nIf you need more details, you can visit the [University of Osnabrück's Biology Department website](https://www.uni-osnabrueck.de/studieninteressierte/studiengaenge-a-z/biologiebiology-from-molecules-to-organisms-master-of-science/) for additional information.\n\nIf you have any other questions or need further assistance, feel free to ask!"
-            
+
         # extract the response from the session state
-        response = at.session_state['messages'][4]['content']
+        response = at.session_state["messages"][4]["content"]
         evaluator = load_evaluator("pairwise_embedding_distance")
-        score = evaluator.evaluate_string_pairs(prediction=response, prediction_b=expected_output)
+        score = evaluator.evaluate_string_pairs(
+            prediction=response, prediction_b=expected_output
+        )
 
-        self.assertGreaterEqual(score['score'], 0, "The value is not greater than or equal to 0.")
-        self.assertIn('Biology' or 'biology', response)
+        self.assertGreaterEqual(
+            score["score"], 0, "The value is not greater than or equal to 0."
+        )
 
-     
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    
     unittest.main()
     print()
