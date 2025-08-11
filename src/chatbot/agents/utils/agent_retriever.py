c565bd9f4881b101d4a82fc878bfcba87c8f43b1
yesidc <yesidcanoc@gmail.com>
2025-08-11 12:37:34 +0000

    feat: implement retrieval functions for Infinity-RAGFlow and Milvus with improved error handling

--- src/chatbot/agents/utils/agent_retriever.py
+++ src/chatbot/agents/utils/agent_retriever.py
@@ -1,66 +1,118 @@
 import os
+from typing import List, NamedTuple
 
-from src.chatbot.db.clients import milvus_client
+from src.chatbot.db.milvus_client import MilvusSingleton
+from src.chatbot.db.ragflow_client import RAGFlowSingleton
 from src.chatbot.embeddings.main import get_embeddings
 from src.chatbot_log.chatbot_logger import logger
+from src.config.core_config import settings
 
 HIS_IN_ONE_COLLECTON = "troubleshooting"
 EXAMINATION_REGULATIONS_COLLECTION = "examination_regulations"
 DOCUMENT_SEPARATOR = "\n\n"
 NOT_FOUND_MESSAGE = "Result: No documents found"
 
-search_params = {
-    "metric_type": "L2",
-    "offset": 0,
-    "ignore_growing": False,
-    "params": {"nprobe": 10},
-}
+
+# TODO: Rewrite this module, provide an API to retrieve using Milvus or Infinity
+
+VECTOR_DB_TYPE = settings.vector_db_settings.type
 
 
-def _get_relevant_documents(
-    query: str,
-    filter_program_name: str,
-) -> tuple[str, list[tuple[str, int]]]:
-    # TODO: add a filter for the program name WHEN searching
+class Reference(NamedTuple):
+    source: str
+    page: int
+    doc_id: str | None = None
 
-    ref = []
 
+def retrieve_from_infinity_ragflow(collection_name: str, query: str):
+    ref: list[Reference] = []
+    results = []
+    try:
+
+        ragflow_client = RAGFlowSingleton()
+        chunks = ragflow_client.get_chunks(query, collection_name)
+        for chunk in chunks:
+            source = chunk.document_keyword
+            page = chunk.page
+            ref.append(Reference(source, page, chunk.id))
+            results.append(f"Source: {source} \nText: {chunk.content}")
+        return DOCUMENT_SEPARATOR.join(results), ref
+    except Exception as e:
+        logger.error(f"[RAGFlow]Error during similarity search: {e}")
+        return NOT_FOUND_MESSAGE, ref
+
+
+def retrieve_from_milvus(collection_name: str, query: str, doc_search_params: dict):
+
+    search_params = {
+        "metric_type": "L2",
+        "offset": 0,
+        "ignore_growing": False,
+        "params": {"nprobe": 10},
+    }
+    milvus_client = MilvusSingleton()
     try:
 
         # test if the collection is loaded
-        loaded = milvus_client.client.get_load_state(
-            collection_name=EXAMINATION_REGULATIONS_COLLECTION
-        )
+        loaded = milvus_client.client.get_load_state(collection_name=collection_name)
         if loaded["state"].name != "Loaded":
             logger.warning(
-                f"[VECTOR DB]Collection {EXAMINATION_REGULATIONS_COLLECTION} is not loaded. Current state: {loaded}"
+                f"[VECTOR DB]Collection {collection_name} is not loaded. Current state: {loaded}"
             )
 
         # get query vector
         query_vector = get_embeddings(query)
 
         docs = milvus_client.client.search(
-            collection_name=EXAMINATION_REGULATIONS_COLLECTION,
-            output_fields=["pk", "source", "text", "page"],
+            collection_name=collection_name,
             data=[query_vector],
             search_params=search_params,
-            filter=f"source LIKE '%{filter_program_name}%'",
+            **doc_search_params,
         )
-
-        results = []
-
-        # TODO: IMPLEMENT A RERANKING FUNCTION
-
-        for doc in docs[0]:
-            # TODO consider moving this to the graph state
-            source = os.path.basename(doc["entity"]["source"])
-            page = doc["entity"]["page"]
-            ref.append((source, page))
-            results.append(f'Source: {source} \nText: {doc["entity"]["text"]}')
-
-        return DOCUMENT_SEPARATOR.join(results), ref
+        return docs
     except Exception as e:
         logger.error(f"[VECTOR DB]Error during similarity search: {e}")
+        raise e
+
+
+# used by the examination regulations tool
+def _get_relevant_documents(
+    query: str,
+    filter_program_name: str,
+) -> tuple[str, list[Reference]]:
+    # TODO: add a filter for the program name WHEN searching
+
+    ref: list[Reference] = []
+    results = []
+
+    if VECTOR_DB_TYPE == "Milvus":
+
+        try:
+            doc_search_params = {
+                "output_fields": ["pk", "source", "text", "page"],
+                "filter": f"source LIKE '%{filter_program_name}%'",
+            }
+
+            docs = retrieve_from_milvus(
+                EXAMINATION_REGULATIONS_COLLECTION, query, doc_search_params
+            )
+            # TODO: IMPLEMENT A RERANKING FUNCTION
+
+            for doc in docs[0]:
+                # TODO consider moving this to the graph state
+                source = os.path.basename(doc["entity"]["source"])
+                page = doc["entity"]["page"]
+                ref.append(Reference(source, page))
+                results.append(f'Source: {source} \nText: {doc["entity"]["text"]}')
+
+            return DOCUMENT_SEPARATOR.join(results), ref
+        except Exception as e:
+            return NOT_FOUND_MESSAGE, ref
+    elif VECTOR_DB_TYPE == "Infinity-RAGFlow":
+
+        return retrieve_from_infinity_ragflow(EXAMINATION_REGULATIONS_COLLECTION, query)
+    else:
+        logger.error(f"[VECTOR DB]Unsupported vector DB type: {VECTOR_DB_TYPE}")
         return NOT_FOUND_MESSAGE, ref
 
 
@@ -68,25 +120,13 @@ def retriever_his_in_one(query: str) -> str:
 
     try:
 
-        # test if the collection is loaded
-        loaded = milvus_client.client.get_load_state(
-            collection_name=HIS_IN_ONE_COLLECTON
-        )
-        if loaded["state"].name != "Loaded":
-            logger.warning(
-                f"[VECTOR DB]Collection {HIS_IN_ONE_COLLECTON} is not loaded. Current state: {loaded}"
-            )
-        # get query vector
-        query_vector = get_embeddings(query)
-
-        docs = milvus_client.client.search(
-            collection_name=HIS_IN_ONE_COLLECTON,
-            output_fields=["pk", "text"],
-            data=[query_vector],
-            search_params=search_params,
-        )
+        doc_search_params = {
+            "output_fields": ["pk", "text"],
+        }
 
         results = []
+
+        docs = retrieve_from_milvus(HIS_IN_ONE_COLLECTON, query, doc_search_params)
 
         for doc in docs[0]:
 
@@ -95,5 +135,4 @@ def retriever_his_in_one(query: str) -> str:
         return DOCUMENT_SEPARATOR.join(results)
 
     except Exception as e:
-        logger.error(f"[VECTOR DB]Error during similarity search: {e}")
         return NOT_FOUND_MESSAGE
